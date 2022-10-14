02c2fd17a8b7cd7e13dc4d542b2ae5ef80806f69
Fernaldy-ASD <Fernaldy.Anggara-1@student.uts.edu.au>
2022-10-14 19:45:05 +1100

    Fix

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,5 +1,42 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+//Interface
+public interface IDateTimeProvider
+{
+    DateTime GetNow();
+}
+
+//Implementation with real DateTime.Now
+public class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime GetNow() => DateTime.Now;
+}
+
+//DI-Container registration
+services.AddScoped<IDateTimeProvider, DateTimeProvider>();
+
+//App logic 
+public class User
+{
+    public User(IDateTimeProvider dateTimeProvider)
+    { 
+        CreatedAt = dateTimeProvider.GetNow();
+    }
+
+    public DateTime CreatedAt { get; }
+}
+
+//FixedDateTimeProvider implementation for unit tests
+public class FixedDateTimeProvider : IDateTimeProvider
+{
+    private DateTime _fixedDateTime;
+
+    public FixedDateTimeProvider(DateTime fixedDateTime)
+        => _fixedDateTime = fixedDateTime;
+
+    public DateTime GetNow() => _fixedDateTime;
+}
+
 namespace TestProject1
 {
     [TestClass]
@@ -14,7 +51,9 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.AreEqual(Program.addNumbers(3, 4), 7);
+            User user = new User(new FixedDateTimeProvider(new DateTime(2021, 07, 20)));
+
+            Assert.Equal(new DateTime(2021, 07, 20), user.CreatedAt);
         }
 
     }
