ae78adc1907fc5eae39a6133f8feaadac9b11a30
Fernaldy-ASD <Fernaldy.Anggara-1@student.uts.edu.au>
2022-10-14 19:23:36 +1100

    Time

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,5 +1,40 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+public interface ISystemClock
+{
+    DateTime Now { get; }
+}
+
+public class SystemClock : ISystemClock
+{
+    public DateTime Now
+        => DateTime.Now;
+}
+
+public class CreditCardValidator : AbstractValidator<CreditCard>
+{
+    public CreditCardValidator(ISystemClock systemClock)
+    {
+        var now = systemClock.Now;
+        // Beep, beep, boop
+        // Rest of the code here...
+    }
+}
+
+public class FixedDateClock : ISystemClock
+{
+    private readonly DateTime _when;
+
+    public FixedDateClock(DateTime when)
+    {
+        _when = when;
+    }
+
+    public DateTime Now
+        => _when;
+}
+
+
 namespace TestProject1
 {
     [TestClass]
@@ -11,19 +46,23 @@ namespace TestProject1
             Assert.AreEqual(Program.addNumbers(2, 3), 5);
         }
         
-    public static class SystemTime
-    {
-        public static Func<DateTime> Now = () => DateTime.Now;
-    }
+   
 
         [TestMethod]
         public void TestMethod2()
         {
-            SystemTime.Now = () => new DateTime(2000,1,1);
-            repository.ResetFailures(failedMsgs); 
-            SystemTime.Now = () => new DateTime(2000,1,2);
-            var msgs = repository.GetAllReadyMessages(); 
-            Assert.AreEqual(2, msgs.Length);
+            var when = new DateTime(2021, 01, 01);
+            var clock = new FixedDateClock(when);
+        // This time we're passing a fake clock implementation
+            var validator = new CreditCardValidator(clock);
+        //                                      ^^^^^
+
+            var request = new CreditCardBuilder()
+                        .WithExpirationYear(when.AddYears(-1).Year)
+                        .Build();
+            var result = validator.TestValidate(request);
+
+            result.ShouldHaveAnyValidationError();
         }
 
     }
