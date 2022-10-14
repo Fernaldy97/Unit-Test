755108ccf8779e40122698ce89328284c747dbaf
Fernaldy-ASD <Fernaldy.Anggara-1@student.uts.edu.au>
2022-10-14 20:17:37 +1100

    Check

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,4 +1,52 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calendar
+{
+    public class EventChecker
+    {
+        private IDateTimeProvider _dateTimeProvider;
+
+        public EventChecker(IDateTimeProvider dateTimeProvider)
+        {
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public bool CanScheduleEvent()
+        {
+            return _dateTimeProvider.GetCurrentTime().DayOfWeek == DayOfWeek.Saturday
+                || _dateTimeProvider.GetCurrentTime().DayOfWeek == DayOfWeek.Sunday;
+        }
+    }
+
+    public interface IDateTimeProvider
+    {
+        DateTime GetCurrentTime();
+    }
+
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+    }
+
+    class FakeDateTimeProvider : IDateTimeProvider
+    {
+        private DateTime _time;
+
+        public FakeDateTimeProvider(DateTime time)
+        {
+            _time = time;
+        }
+
+        public DateTime GetCurrentTime()
+        {
+            return _time;
+        }
+    }
+}
 
 namespace TestProject1
 {
@@ -14,7 +62,7 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod2()
         {
-            var eventChecker = new EventChecker(new FakeDateTimeProvider(new DateTime(2021, 4, 4)));
+            var eventChecker = new Calendar.EventChecker(new Calendar.FakeDateTimeProvider(new DateTime(2021, 4, 4)));
 
             var result = eventChecker.CanScheduleEvent();
 
