b27470814c66e06cc645e2be33c0ed222fd9cb76
Fernaldy-ASD <Fernaldy.Anggara-1@student.uts.edu.au>
2022-10-14 19:29:27 +1100

    Fix

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,40 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-public interface ISystemClock
-{
-    DateTime Now { get; }
-}
-
-public class SystemClock : ISystemClock
-{
-    public DateTime Now
-        => DateTime.Now;
-}
-
-public class CreditCardValidator : AbstractValidator<CreditCard>
-{
-    public CreditCardValidator(ISystemClock systemClock)
-    {
-        var now = systemClock.Now;
-        // Beep, beep, boop
-        // Rest of the code here...
-    }
-}
-
-public class FixedDateClock : ISystemClock
-{
-    private readonly DateTime _when;
-
-    public FixedDateClock(DateTime when)
-    {
-        _when = when;
-    }
-
-    public DateTime Now
-        => _when;
-}
-
-
 namespace TestProject1
 {
     [TestClass]
@@ -45,24 +10,11 @@ namespace TestProject1
         {
             Assert.AreEqual(Program.addNumbers(2, 3), 5);
         }
-        
-   
 
         [TestMethod]
         public void TestMethod2()
         {
-            var when = new DateTime(2021, 01, 01);
-            var clock = new FixedDateClock(when);
-        // This time we're passing a fake clock implementation
-            var validator = new CreditCardValidator(clock);
-        //                                      ^^^^^
-
-            var request = new CreditCardBuilder()
-                        .WithExpirationYear(when.AddYears(-1).Year)
-                        .Build();
-            var result = validator.TestValidate(request);
-
-            result.ShouldHaveAnyValidationError();
+            Assert.AreEqual(Program.addNumbers(3, 4), 7);
         }
 
     }
