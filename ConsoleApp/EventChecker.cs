308baa8fd35c4b5e9d56bec1c210acbdc7eb7153
Fernaldy-ASD <Fernaldy.Anggara-1@student.uts.edu.au>
2022-10-14 20:09:57 +1100

    Check

---
+++ ConsoleApp/EventChecker.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Calendar
+{
+    public class EventChecker
+    {
+        private IDateTimeProvider _dateTimeProvider;
+
+        public EventChecker(IDateTimeProvider dateTimeProvider)
+        {
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public bool CanScheduleEvent()
+        {
+            return _dateTimeProvider.GetCurrentTime().DayOfWeek == DayOfWeek.Saturday
+                || _dateTimeProvider.GetCurrentTime().DayOfWeek == DayOfWeek.Sunday;
+        }
+    }
+
+    public interface IDateTimeProvider
+    {
+        DateTime GetCurrentTime();
+    }
+
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime GetCurrentTime()
+        {
+            return DateTime.Now;
+        }
+    }
+
+    class FakeDateTimeProvider : IDateTimeProvider
+    {
+        private DateTime _time;
+
+        public FakeDateTimeProvider(DateTime time)
+        {
+            _time = time;
+        }
+
+        public DateTime GetCurrentTime()
+        {
+            return _time;
+        }
+    }
+}
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,21 +1,4 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-//DateTimeProvider implementation
-public static class DateTimeProvider
-{
-    private static Func<DateTime> _dateTimeNowFunc = () => DateTime.Now;
-    public static DateTime Now => _dateTimeNowFunc();
-
-    public static void Set(Func<DateTime> dateTimeNowFunc)
-    {
-        _dateTimeNowFunc = dateTimeNowFunc;
-    }
-}
-
-//DateTimeProvider usage in app logic
-public class User
-{
-    public DateTime CreatedAt { get; } = DateTimeProvider.Now;
-}
 
 namespace TestProject1
 {
@@ -31,9 +14,11 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod2()
         {
-           DateTimeProvider.Set(() => new DateTime(2021, 07, 20));
-           var user = new User();
-           Assert.Equal(new DateTime(2021, 07, 20), user.CreatedAt);
+            var eventChecker = new EventChecker(new FakeDateTimeProvider(new DateTime(2021, 4, 4)));
+
+            var result = eventChecker.CanScheduleEvent();
+
+            Assert.True(result);
         }
 
     }
